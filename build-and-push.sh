1faa14a23f480a2c4aafd007eb8c4275e1f357c0
avm-ai <info@avm.codes>
2025-06-12 09:06:37 +0200

    target Lambda Platform

--- build-and-push.sh
+++ build-and-push.sh
@@ -7,7 +7,7 @@ aws ecr get-login-password --region eu-north-1 \
   | podman login --username AWS \
   --password-stdin $ECR_BASE
 
-podman build -t $CONTAINER_NAME .
+podman build --platform linux/amd64 -t $CONTAINER_NAME .
 
 podman tag $CONTAINER_NAME:latest \
   $ECR_BASE$ECR_REPOSITORY
